439ee4a108858d1fb0b90a6d2469fc76c1eb2e9b
Unknown-Googly <93320616+Unknown-Googly@users.noreply.github.com>
2021-11-01 01:00:39 +0500

    Initial commit

---
+++ cw1_8567_view/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cw1_8567_view.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
